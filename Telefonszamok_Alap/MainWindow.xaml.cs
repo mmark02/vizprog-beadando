a0e9dfdb2f31eba85f9df746f38fcb12fb8537cb
mmark02 <molnar.mark0801@gmail.com>
2025-04-10 14:43:26 +0200

    0410

--- Telefonszamok_Alap/MainWindow.xaml.cs
+++ Telefonszamok_Alap/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using cnTelefonkonyv;
+using Models;
 using Telefonszamok_Alap.Models;
 
 namespace Telefonszamok_Alap;
@@ -79,6 +80,51 @@ public partial class MainWindow : Window
 
     private void miHelysegekModositas_Click(object sender, RoutedEventArgs e)
     {
+        dgAdatracs.Visibility = Visibility.Hidden;
+        grRacs.Visibility = Visibility.Visible;
 
+        grRacs.DataContext = _context.enHelysegek.ToList();
+        cbIrsz.SelectedItem = 0;
+    }
+
+    private void cbIrsz_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var enAktualis = ((ComboBox)sender).SelectedItem as enHelyseg;
+        cbIrsz.SelectedItem = enAktualis;
+        cbHelysegnev.SelectedItem = enAktualis;
+        tbIrsz.Text = enAktualis.IRSZ.ToString();
+        tbHelysegnev.Text = enAktualis.Nev;
+    }
+
+    private void btRogzit_Click(object sender, RoutedEventArgs e)
+    {
+        var enAktualis = cbIrsz.SelectedItem as enHelyseg;
+        if (!btUjHelyseg.IsEnabled)
+        {
+            enAktualis = new enHelyseg();
+            _context.enHelysegek.Add(enAktualis);
+        }
+        enAktualis.IRSZ = tbIrsz.Text;
+        enAktualis.Nev = tbHelysegnev.Text;
+        grRacs.Visibility = Visibility.Hidden;
+    }
+
+    private void btUjHelyseg_Click(object sender, RoutedEventArgs e)
+    {
+        Beallit(false);
+        tbIrsz.Text = "";
+        tbHelysegnev.Text = "";
+    }
+
+    private void Beallit(bool b)
+    {
+        btUjHelyseg.IsEnabled = b;
+        cbIrsz.IsEnabled = b;
+        cbHelysegnev.IsEnabled = b;
+    }
+
+    private void btVissza_Click(object sender, RoutedEventArgs e)
+    {
+        grRacs.Visibility = Visibility.Hidden;
     }
 }
